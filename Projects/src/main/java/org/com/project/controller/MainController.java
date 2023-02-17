04289a7fcf66e21178a1bfdf4563d33d6e794c84
bulsanix <bulsanix@DESKTOP-AK1L0M8>
2023-02-17 21:01:22 +0900

    완성된 메인컨트롤러

--- Projects/src/main/java/org/com/project/controller/MainController.java
+++ Projects/src/main/java/org/com/project/controller/MainController.java
@@ -1,18 +1,21 @@
 package org.com.project.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+
+
 @Controller
-@RequestMapping(value = "/main", method = RequestMethod.GET)
+@RequestMapping(value="/",method=RequestMethod.GET)
 public class MainController {
-	@RequestMapping(value="home", method = RequestMethod.GET)
-	public String Main(HttpServletRequest request, HttpServletResponse response) {
+	private static final Logger logger=LoggerFactory.getLogger(MainController.class);
+	@RequestMapping(value="/home",method=RequestMethod.GET)
+	public String Main() {
 		return "home";
 	}
 }
